7656590927066d4982ec5861b09db986a2761fad
Corbin <corbindbullard@gmail.com>
2023-10-20 11:56:56 -0400

    basic auth

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,4 +1,8 @@
+import User from "@/app/models/user";
+import { connectMongoDB } from "@/lib/mongodb";
+import { headers } from "@/next.config";
 import GoogleProvider from "next-auth/providers/google";
+import { Content } from "next/font/google";
 export const options = {
     providers: [
         GoogleProvider({
@@ -8,8 +12,31 @@ export const options = {
     ],
     callbacks: {
         async signIn({user, account}) {
-            console.log("User: ", user);
-            console.log("Account: ", account);
+            if (account.provider === 'google') {
+                const { name, email } = user;
+                try {
+                    await connectMongoDB();
+                    const userExists = await User.findOne({ email });
+                    if (!userExists) {
+                        const res = await fetch('http://localhost:3000/api/users', {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                name,
+                                email
+                            })
+                        });
+                        if (res.ok) {
+                            return user;
+                        }
+                    }
+                    else console.log("USER EXISTS");
+                } catch(error) {
+                    console.log(error)
+                }
+            }
             return user;
         }
     },
--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, models } from "mongoose";
 
 const userSchema = new Schema({
     email: {
