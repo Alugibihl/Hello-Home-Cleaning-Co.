7f28fdd606606fd5bc0bcc0538ce83d619e4a952
Corbin <corbindbullard@gmail.com>
2023-10-20 10:25:05 -0400

    env_example

---
+++ app/models/user.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+},
+{timestamps: true }
+);
+const User = models.User || mongoose.model('User', userSchema);
