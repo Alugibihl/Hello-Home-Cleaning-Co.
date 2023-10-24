2387a321c08c4e1bb2b51e016b100581987b593c
alexl <alexandermlugbiihl@gmail.com>
2023-10-24 11:25:07 -0400

    route cleanup start

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -2,47 +2,35 @@ import { connectMongoDB } from "@/lib/mongodb";
 import Appointment from "@/app/models/appointments";
 import { NextResponse } from "next/server";
 
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        try {
-            await connectMongoDB();
-            const newAppointment = new Appointment(req.body);
-            const createdAppointment = await newAppointment.save();
-            res.status(201).json(createdAppointment);
-        } catch (error) {
-            res.status(500).json({ error: "Appointment creation failed" });
-        }
-    } else {
-        res.status(405).json({ error: "Method not allowed" });
+export default async function POST(request) {
+    try {
+        await connectMongoDB();
+        const newAppointment = new Appointment(request.body);
+        const createdAppointment = await newAppointment.save();
+        res.status(201).json(createdAppointment);
+    } catch (error) {
+        res.status(500).json({ error: "Appointment creation failed" });
     }
 }
 
 
-export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        try {
-            await connectMongoDB();
-            const appointments = await Appointment.find().exec();
-            res.status(200).json(appointments);
-        } catch (error) {
-            res.status(500).json({ error: "Appointment retrieval failed" });
-        }
-    } else {
-        res.status(405).json({ error: "Method not allowed" });
+export async function GET(request) {
+    try {
+        await connectMongoDB();
+        const appointments = await Appointment.findById(request.id);
+        return NextResponse.json({ appointments, status: 201 })
+    } catch (error) {
+        return NextResponse.json({ error: "Appointment retrieval failed", status: 500 });
     }
 }
 
-export default async function handler(req, res) {
-    if (req.method === 'PUT') {
-        try {
-            await connectMongoDB();
-            const { _id, ...updateData } = req.body;
-            const updatedAppointment = await Appointment.findByIdAndUpdate(_id, updateData, { new: true }).exec();
-            res.status(200).json(updatedAppointment);
-        } catch (error) {
-            res.status(500).json({ error: "Appointment update failed" });
-        }
-    } else {
-        res.status(405).json({ error: "Method not allowed" });
+export async function PUT(request) {
+    try {
+        await connectMongoDB();
+        const { id, ...updateData } = request.body;
+        const updatedAppointment = await Appointment.findByIdAndUpdate(id, updateData, { new: true }).exec();
+        return NextResponse.json({ updatedAppointment, status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: "Appointment update failed", status: 500 });
     }
 }
