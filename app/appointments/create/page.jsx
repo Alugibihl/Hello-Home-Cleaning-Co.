40ab6f2b35480ea989850a040771189617c525a7
alexl <alexandermlugbiihl@gmail.com>
2023-10-27 14:16:13 -0400

    update form

--- app/appointments/create/page.jsx
+++ app/appointments/create/page.jsx
@@ -47,9 +47,18 @@ async function createAppointment({
 
 export default function Page() {
   const session = useSession();
-  const [name, setName] = useState();
-  const [date, setDate] = useState();
-  const [phone, setPhone] = useState();
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [stories, setStories] = useState("");
+  const [rooms, setRooms] = useState("");
+  const [pets, setPets] = useState("");
+  const [noTouch, setNoTouch] = useState("None");
+  const [focus, setFocus] = useState("None");
+  const [allergies, setAllergies] = useState(false);
+  const [frequency, setFrequency] = useState("none");
+  const [refSource, setRefSource] = useState("");
 
   const userId = session.data?.user?.id;
   const router = useRouter();
@@ -57,7 +66,6 @@ export default function Page() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const frequency = document.querySelector("#frequency").value;
     const appointment = await createAppointment({
       name,
       date,
@@ -79,12 +87,12 @@ export default function Page() {
   };
 
   return (
-    <form onSubmit={handleSubmit} classNameName="w-full max-w-lg ml-6">
+    <form onSubmit={handleSubmit} className="w-full max-w-lg ml-6">
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
           <label
             className="block text-gray-700 text-s font-bold mb-2"
-            for="grid-name"
+            htmlFor="grid-name"
           >
             Name
           </label>
@@ -92,7 +100,7 @@ export default function Page() {
             value={name}
             type="text"
             onChange={(e) => setName(e.target.value)}
-            classNameName="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           />
         </div>
       </div>
@@ -100,7 +108,7 @@ export default function Page() {
         <div className="w-full px-3">
           <label
             className="block text-gray-700 text-s font-bold mb-2"
-            for="grid-date"
+            htmlFor="grid-date"
           >
             Date
           </label>
@@ -108,7 +116,7 @@ export default function Page() {
             value={date}
             type="date"
             onChange={(e) => setDate(e.target.value)}
-            classNameName="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           />
         </div>
       </div>
@@ -116,7 +124,7 @@ export default function Page() {
         <div className="w-full px-3">
           <label
             className="block text-gray-700 text-s font-bold mb-2"
-            for="grid-phone"
+            htmlFor="grid-phone"
           >
             Phone Number
           </label>
@@ -124,22 +132,160 @@ export default function Page() {
             value={phone}
             type="text"
             onChange={(e) => setPhone(e.target.value)}
-            classNameName="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-address"
+          >
+            Address
+          </label>
           <input
-            type="select"
+            value={address}
+            type="text"
+            onChange={(e) => setAddress(e.target.value)}
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-stories"
+          >
+            Stories
+          </label>
+          <input
+            value={stories}
+            type="text"
+            onChange={(e) => setStories(e.target.value)}
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-rooms"
+          >
+            Rooms
+          </label>
+          <input
+            value={rooms}
+            type="text"
+            onChange={(e) => setRooms(e.target.value)}
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-pets"
+          >
+            Pets
+          </label>
+          <input
+            value={pets}
+            type="text"
+            onChange={(e) => setPets(e.target.value)}
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-noTouch"
+          >
+            No Touch
+          </label>
+          <input
+            value={noTouch}
+            type="text"
+            onChange={(e) => setNoTouch(e.target.value)}
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-focus"
+          >
+            Focus
+          </label>
+          <input
+            value={focus}
+            type="text"
+            onChange={(e) => setFocus(e.target.value)}
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-allergies"
+          >
+            Allergies
+          </label>
+          <input
+            type="checkbox"
+            checked={allergies}
+            onChange={(e) => setAllergies(e.target.checked)}
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-frequency"
+          >
+            Frequency
+          </label>
+          <select
             id="frequency"
             name="frequency"
-            classNameName="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            value={frequency}
+            onChange={(e) => setFrequency(e.target.value)}
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           >
             <option value="2">Every 2 weeks</option>
             <option value="3">Every 3 weeks</option>
             <option value="4">Every 4 weeks</option>
             <option value="none">None</option>
-          </input>
+          </select>
         </div>
       </div>
-      <button classNameName="mb-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block text-gray-700 text-s font-bold mb-2"
+            htmlFor="grid-refSource"
+          >
+            Reference Source
+          </label>
+          <input
+            value={refSource}
+            type="text"
+            onChange={(e) => setRefSource(e.target.value)}
+            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </div>
+      </div>
+      <button className="mb-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
         Create Appointment
       </button>
     </form>
