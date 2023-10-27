a0d1a0d4d04283678e2f5a919344e634032c3002
Corbin <corbindbullard@gmail.com>
2023-10-27 19:09:34 -0400

    cleaner

--- app/api/appointments/route.js
+++ app/api/appointments/route.js
@@ -5,30 +5,64 @@ import { getServerSession } from "next-auth";
 import { options } from "../auth/[...nextauth]/options";
 import { AuthOptions } from "next-auth";
 
-
 export async function POST(request) {
-    const headers = request.headers.get("authorization");
-    const user = JSON.parse(headers);
-    console.log("user", user, "id", user.id)
-    try {
-        const { name, date, phone, address, stories, rooms, pets, noTouch, focus, allergies, frequency, refSource } = await request.json()
-        await connectMongoDB();
-        await Appointment.create({ name, date, phone, userId: user.id, address, stories, rooms, pets, noTouch, focus, allergies, frequency, refSource });
-        return NextResponse.json({ message: "Appointment Created", status: 201 });
-    } catch (error) {
-        return NextResponse.json({ error: "Appointment creation failed", status: 500 });
-    }
+  const headers = request.headers.get("authorization");
+  const user = JSON.parse(headers);
+  console.log("user", user, "id", user.id);
+  try {
+    const {
+      name,
+      date,
+      phone,
+      address,
+      stories,
+      rooms,
+      pets,
+      noTouch,
+      focus,
+      allergies,
+      frequency,
+      refSource,
+    } = await request.json();
+    await connectMongoDB();
+    await Appointment.create({
+      name,
+      date,
+      phone,
+      userId: user.id,
+      address,
+      stories,
+      rooms,
+      pets,
+      noTouch,
+      focus,
+      allergies,
+      frequency,
+      refSource,
+    });
+    return NextResponse.json({ message: "Appointment Created", status: 201 });
+  } catch (error) {
+    return NextResponse.json({
+      error: "Appointment creation failed",
+      status: 500,
+    });
+  }
 }
 
-
 export async function GET(request) {
-    const session = await getServerSession(options)
-    console.log("----------session___________________________________________", session)
-    try {
-        await connectMongoDB();
-        const appointments = await Appointment.find();
-        return NextResponse.json({ appointments })
-    } catch (error) {
-        return NextResponse.json({ error: "Appointment retrieval failed", status: 500 });
-    }
+  const session = await getServerSession(options);
+  console.log(
+    "----------session___________________________________________",
+    session
+  );
+  try {
+    await connectMongoDB();
+    const appointments = await Appointment.find();
+    return NextResponse.json({ appointments });
+  } catch (error) {
+    return NextResponse.json({
+      error: "Appointment retrieval failed",
+      status: 500,
+    });
+  }
 }
