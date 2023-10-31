21ba341b2de691a09e327840b45772edd839b22b
Francisco Palacios <Fpalacios926@gmail.com>
2023-10-31 01:57:09 -0700

    inital stylings for JOT

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -6,10 +6,41 @@ import ResumeForm from "@/components/Resume/ResumeForm"
 export default function CareersPage() {
     return (
         <>
-            <h1>Join Our Team</h1>
-            <h2>BLAH BLAH BLAH</h2>
-            <h3>BLAH BLAH BLAH</h3>
-            <h4>BLAH BLAH BLAH</h4>
-            <ResumeForm />
+            <div className="bg-blue-200">
+                <div className="flex justify-center">
+                    <h1 className="text-6xl">Join Our Team</h1>
+                </div>
+                <div className="flex bg-slate-200 justify-around">
+                    <div>
+                        <h2>BLAH BLAH BLAH</h2>
+                        <img src="/pexels-karolina-grabowska-4239091.jpg" className="w-max h-52" />
+                    </div>
+                    <div>
+                        <h2 className="flex justify-center">BLAH BLAH BLAH</h2>
+                        <img src="/pexels-tima-miroshnichenko-6195122.jpg" className="w-max h-52" />
+                    </div>
+                    <div>
+
+                        <h2>BLAH BLAH BLAH</h2>
+                        <img src="/pexels-polina-zimmerman-4008518.jpg" className="w-max h-52" />
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <h3>We are looking for experienced cleaners to join our team </h3>
+                </div>
+                <div className="flex flex-col">
+                    <p>Stuff about the cleaning business that they offer</p>
+                    <ul>
+                        <li>benefits 1</li>
+                        <li>benefits 2</li>
+                        <li>benefits 3</li>
+                    </ul>
+                </div>
+                <div className="flex justify-center">
+                    <ResumeForm />
+                </div>
+                {/* <button>Apply Now</button> */}
+            </div>
+
         </>)
 }
--- components/Resume/ResumeForm.jsx
+++ components/Resume/ResumeForm.jsx
@@ -15,32 +15,32 @@ export default function ResumeForm() {
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
                     <label
-                    htmlFor="email"
-                    className="block text-gray-700 text-s font-bold mb-2"
+                        htmlFor="email"
+                        className="block text-gray-700 text-s font-bold mb-2"
                     >
-                    Email Address
+                        Email Address
 
-                    <input
-                        id="email"
-                        type="email"
-                        name="email"
-                        required="true"
-                        className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                            required="true"
+                            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
 
                         />
                     </label>
                     <ValidationError
                         prefix="Email"
                         field="email"
-                        errors={state.errors}/>
+                        errors={state.errors} />
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
                     <label
-                    className="block text-gray-700 text-s font-bold mb-2">
-                         Message
-                         <textarea
+                        className="block text-gray-700 text-s font-bold mb-2">
+                        Message
+                        <textarea
                             id="message"
                             name="message"
                             className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -48,12 +48,12 @@ export default function ResumeForm() {
                         <ValidationError
                             prefix="Message"
                             field="message"
-                            errors={state.errors}/>
+                            errors={state.errors} />
                     </label>
                 </div>
             </div>
             {/* OTHER QUESTIONS / WORK RELATED Q  */}
-                {/* <label for="resume">Resume
+            {/* <label for="resume">Resume
                 <input
                     id="resume"
                     type="file"
@@ -66,7 +66,8 @@ export default function ResumeForm() {
                     />
                 </label> */}
             <button className="mb-6 bg-blue-500 w-72 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit" disabled={state.submitting}>
+                type="submit"
+                disabled={state.submitting}>
                 Submit
             </button>
 
