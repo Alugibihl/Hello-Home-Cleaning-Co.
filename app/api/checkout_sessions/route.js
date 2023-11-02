9560ba195a9a7a870d801f5aebb094bf8f65a62f
alexl <alexandermlugbiihl@gmail.com>
2023-11-02 18:39:52 -0400

    modded route

--- app/api/checkout_sessions/route.js
+++ app/api/checkout_sessions/route.js
@@ -132,7 +132,7 @@
 
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req) {
     console.log("------------------------------------------ look at this route")
