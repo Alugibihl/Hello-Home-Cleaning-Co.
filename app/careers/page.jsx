7e7e2f31165f9a4e1399c49929232282590806e0
Francisco Palacios <Fpalacios926@gmail.com>
2023-11-04 19:36:14 -0700

    fixed styling

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -17,35 +17,27 @@ export default function CareersPage() {
 
     return (
         <>
-            <div className="bg-blue-200" >
-                <div className="flex justify-center">
-                    <div className="text-6xl">Join Our Team</div>
-                </div>
-                <div className="flex bg-slate-200 justify-around h-72">
-                    <div className="flex flex-col justify-center items-center">
-                        <h2>BLAH BLAH BLAH</h2>
-                        <img className="rounded w-max h-52" src="/pexels-karolina-grabowska-4239091.jpg" />
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <h2 >BLAH BLAH BLAH</h2>
-                        <img src="/pexels-tima-miroshnichenko-6195122.jpg" className="rounded w-max h-52" />
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <h2>BLAH BLAH BLAH</h2>
-                        <img src="/pexels-polina-zimmerman-4008518.jpg" className="rounded w-max h-52" />
+            <div className="" >
+                {/* <div className="flex justify-center">
+                    <div className="text-6xl font-serif">Join Our Team</div>
+                </div> */}
+                <div className="relative w-full">
+                    <img src="/join3.jpg" className="h-fit min-w-full"></img>
+                    <div className="absolute top-1/3 left-0 right-0 mx-auto text-center">
+                        < h1 className="text-6xl text-white font-serif">
+                            Join Our Team
+                        </h1>
                     </div>
                 </div>
-
-                <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-center">We are looking for hardworking, reliable, and positive individuals to join our growing team of cleaning specialists. </h3>
-                    <p>Stuff about the cleaning business that they offer</p>
-                    <ul>
-                        <li>benefits 1 Hours starting at part time with flexiblity to meet your busy life.</li>
-                        <li>benefits 2 No prior experience required. </li>
-                        <li>benefits 3</li>
+                <div className=" font-serif flex flex-col justify-center items-center bg-light-blue pb-32">
+                    <h2 className="text-center  text-2xl">
+                        We are looking for hardworking, reliable, and positive individuals to join our growing team of cleaning specialists </h2>
+                    <ul className="text-xl text-center p-5">
+                        <li>Hours starting at part time with flexiblity to meet your busy life.</li>
+                        <li>No prior experience required. </li>
+                        <li></li>
                     </ul>
-                    {/* //Modal */}
-                    <button onClick={handleModal} className="mb-6 bg-blue-500 w-26 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Apply here</button>
+                    <button onClick={handleModal} className="mt-6 bg-feather-blue hover:bg-highlight-orange font-bold py-2 px-4 rounded">Apply here</button>
                 </div>
                 {showApply && (
                     <Modal
@@ -60,3 +52,15 @@ export default function CareersPage() {
 
         </>)
 }
+{/* <div className="flex flex-col justify-center items-center">
+                        <h2>BLAH BLAH BLAH</h2>
+                        <img className="rounded w-max h-52" src="/pexels-karolina-grabowska-4239091.jpg" />
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <h2 >BLAH BLAH BLAH</h2>
+                        <img src="/pexels-tima-miroshnichenko-6195122.jpg" className="rounded w-max h-52" />
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <h2>BLAH BLAH BLAH</h2>
+                        <img src="/pexels-polina-zimmerman-4008518.jpg" className="rounded w-max h-52" />
+                    </div> */}
