d8dabd4808b31a3f952223ae21915aa216ebcb78
Vincent Minicozzi <vinicozzimincent@gmail.com>
2023-11-05 13:58:00 -0500

    add dropdown for user

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
-"use client";
+"use client"
 import { signIn, signOut } from "next-auth/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -12,30 +12,47 @@ import Modal from "../Modal/Modal";
 const Navbar = ({ session }) => {
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showSignupModal, setShowSignupModal] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const modalFunctions = {
-    setShowLoginModal: (shown) => setShowLoginModal(shown),
-    setShowSignupModal: (shown) => setShowSignupModal(shown),
+    setShowLoginModal,
+    setShowSignupModal,
   };
 
-  const handleSignin = () => {
-    setShowLoginModal(true);
-  };
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (!event.target.closest("#user-menu-button") && !event.target.closest("#user-menu-dropdown")) {
+        setShowDropdown(false);
+      }
+    };
 
-  console.log("NAV SESSION: ", session);
+    if (showDropdown) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      if (showDropdown) {
+        document.removeEventListener("mousedown", handleOutsideClick);
+      }
+    };
+  }, [showDropdown]);
+
+  const appointmentsLink = session?.user.role === 'admin' ? "/admin/appointments" : "/appointments";
+  const appointmentsText = session?.user.role === 'admin' ? "ALL APPOINTMENTS" : "MY APPOINTMENTS";
+
   return (
     <>
-      <nav className="flex items-center justify-between">
-        <Link href="/">
-          <Image
-            src="/hhlogo.png"
-            className="w-56 m-6 ml-20"
-            width={300}
-            height={30}
-            alt="Hello Home Cleaning Co. Logo"
-          ></Image>
+      <nav className="flex items-center justify-between shadow-md border-b-2 py-4 px-6 bg-white">
+        <Link href="/" passHref>
+            <Image
+              src="/hhlogo.png"
+              className="w-36 cursor-pointer"
+              width={144}
+              height={24}
+              alt="Hello Home Cleaning Co. Logo"
+            />
         </Link>
-        <ul className="flex font-roboto font-bold text-base mr-12">
+        <ul className="flex items-center font-roboto font-bold text-md text-gray-700 space-x-4 lg:space-x-6 ">
           <li className="mx-6">
             <Link href="/">HOME</Link>
           </li>
@@ -45,66 +62,75 @@ const Navbar = ({ session }) => {
           <li className="mx-6">
             <Link href="/faq">FAQ</Link>
           </li>
-          {/* <li className="group relative">
-            <div>Services</div>
-            <ol className="nav-hover">
-              <li>
-                <Link href="/services">Our Services</Link>
-              </li>
-              <li>
-                <Link href="preparation">Your first clean</Link>
-              </li>
-              <li>
-                <Link href="/contact">Request a Quote</Link>
-              </li>
-            </ol>
-          </li> */}
           <li className="mx-6">
-            <Link href="/appointments/create">REQUEST QUOTE</Link>
+            <Link href="/careers">JOIN OUR TEAM!</Link>
+          </li>
+          <li className="mx-6">
+            <Link href="/appointments/create">MAKE AN APPOINTMENT</Link>
           </li>
           {session && (
-            <Link
-              href={
-                session?.user?.role === "admin"
-                  ? "/admin/appointments"
-                  : "/appointments"
-              }
-            >
-              {session?.user?.role === "admin"
-                ? "ALL APPOINTMENTS"
-                : "MY APPOINTMENTS"}
-            </Link>
+            <li className="mx-6">
+              <Link href={appointmentsLink}>{appointmentsText}</Link>
+            </li>
           )}
-          <li className="mx-6">
-            {!session ? (
-              // <Link href="/signIn">Sign In</Link>
-              <button onClick={handleSignin} className="">
-                SIGN IN
-              </button>
-            ) : (
-              <div>
-                <p>{session.user.name}</p>
-                <p>{session.user.email}</p>
-                <button onClick={signOut}>Sign Out</button>
+        </ul>
+        {session ? (
+          <div className="relative">
+            <button
+              id="user-menu-button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+              aria-haspopup="true"
+              aria-expanded={showDropdown ? "true" : "false"}
+              onClick={() => setShowDropdown(!showDropdown)}
+            >
+              {session.user.name}
+              <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+              </svg>
+            </button>
+            
+            {showDropdown && (
+              <div id="user-menu-dropdown" className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 z-10">
+                <ul className="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButton">
+                  {/* Include any other dropdown items here */}
+                  <li>
+                    <button
+                      className="block px-4 py-2 hover:bg-gray-100 w-full text-left"
+                      onClick={() => {
+                        signOut();
+                        setShowDropdown(false);
+                      }}
+                    >
+                      Sign out
+                    </button>
+                  </li>
+                </ul>
               </div>
             )}
-          </li>
-        </ul>
+          </div>
+          ) : (
+          <button
+            onClick={() => setShowLoginModal(true)}
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center focus:outline-none focus:ring focus:border-blue-300"
+          >
+            Sign in
+          </button>
+        )}
+        {showLoginModal && (
+          <Modal
+            component={LoginModal}
+            close={() => setShowLoginModal(false)}
+            modalFunctions={modalFunctions}
+          />
+        )}
+        {showSignupModal && (
+          <Modal
+            component={SignupModal}
+            close={() => setShowSignupModal(false)}
+            modalFunctions={modalFunctions}
+          />
+        )}
       </nav>
-      {showLoginModal && (
-        <Modal
-          component={LoginModal}
-          close={() => setShowLoginModal(false)}
-          modalFunctions={modalFunctions}
-        />
-      )}
-      {showSignupModal && (
-        <Modal
-          component={SignupModal}
-          close={(shown) => setShowSignupModal(shown)}
-          modalFunctions={modalFunctions}
-        />
-      )}
     </>
   );
 };
