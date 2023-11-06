289e6b7e36129718f9319f182432413436a29517
Francisco Palacios <Fpalacios926@gmail.com>
2023-11-05 18:42:44 -0800

    added set picture to add and edit

--- components/TeamMember/AddTeamM/AddTeamMember.jsx
+++ components/TeamMember/AddTeamM/AddTeamMember.jsx
@@ -19,21 +19,24 @@ export default function AddTeamMember() {
 
 
     const router = useRouter();
+    console.log(file)
 
-    useEffect(() => {
+    const check = (file) => {
         if (file) {
             console.log(edgestore.myPublicsImages)
             edgestore.myPublicsImages.upload({ file })
                 .then(res => setImg(res.url))
             //save your data here
         }
+        return true
+    }
 
 
-    }, [file])
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        // check(file)
         const res = await fetch("/api/teammembers", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -48,7 +51,7 @@ export default function AddTeamMember() {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="w-full max-w-lg ml-6">
+        <form onSubmit={handleSubmit} className="session-form">
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
                     <label
@@ -94,11 +97,10 @@ export default function AddTeamMember() {
                         <textarea
                             name="about"
                             required={true}
-
                             value={about}
                             type="text"
                             onChange={(e) => setAbout(e.target.value)}
-                            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            className="resize-none h-40 appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                         />
                     </label>
                 </div>
@@ -119,9 +121,13 @@ export default function AddTeamMember() {
                             className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                         />
                     </label>
-
                 </div>
             </div>
+            <button
+                type="button"
+                onClick={async () => check(file)}
+                className="mb-6 justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Set Picture
+            </button>
             <button type="submit" className="mb-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                 Create Team Member
             </button>
--- components/TeamMember/EditTeamM/EditTeamMemeber.jsx
+++ components/TeamMember/EditTeamM/EditTeamMemeber.jsx
@@ -27,7 +27,23 @@ export default function EditTeamMember({ id, name, img, about, position }) {
 
     const router = useRouter();
 
-    useEffect(() => {
+    // useEffect(() => {
+    //     if (file) {
+    //         console.log(edgestore.myPublicsImages, img)
+    //         edgestore.myPublicsImages.upload({
+    //             file,
+    //             options: {
+    //                 replaceTargetUrl: img,
+    //             },
+    //         }).then(res => (setNewImg(res.url)))
+    //         //save your data here
+    //     }
+
+
+    // }, [file,
+    //     // newImg
+    // ])
+    const check = (file) => {
         if (file) {
             console.log(edgestore.myPublicsImages, img)
             edgestore.myPublicsImages.upload({
@@ -38,11 +54,7 @@ export default function EditTeamMember({ id, name, img, about, position }) {
             }).then(res => (setNewImg(res.url)))
             //save your data here
         }
-
-
-    }, [file,
-        // newImg
-    ])
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -52,7 +64,7 @@ export default function EditTeamMember({ id, name, img, about, position }) {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="w-full max-w-lg ml-6">
+        <form onSubmit={handleSubmit} className="session-form">
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
                     <label
@@ -66,22 +78,6 @@ export default function EditTeamMember({ id, name, img, about, position }) {
                             type="text"
                             onChange={(e) => setNewName(e.target.value)}
                             className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        />
-                    </label>
-                </div>
-            </div>
-
-            <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                    <label
-                        className="block text-gray-700 text-s font-bold mb-2"
-                    >
-                        About
-                        <textarea
-                            name="about"
-                            value={newAbout}
-                            type="text"
-                            onChange={(e) => setNewAbout(e.target.value)}
                         />
                     </label>
                 </div>
@@ -108,40 +104,26 @@ export default function EditTeamMember({ id, name, img, about, position }) {
                 <div className="w-full px-3">
                     <label
                         className="block text-gray-700 text-s font-bold mb-2"
-
                     >
                         About
                         <textarea
-                            required={true}
                             name="about"
+                            required={true}
                             value={newAbout}
                             type="text"
                             onChange={(e) => setNewAbout(e.target.value)}
-                            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            className="resize-none h-40 appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                         />
-                        {/* <button
-                            className="mb-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="button" onClick={async => {
-                                if (file) {
-                                    console.log(edgestore.myPublicsImages, img)
-                                    edgestore.myPublicsImages.upload({
-                                        file,
-                                        options: {
-                                            replaceTargetUrl: img,
-                                        },
-                                    }).then(res => (setNewImg(res.url)))
-                                    //save your data here
-                                }
-                            }}> set Picture</button> */}
-                        {/* <img src={newImg ? newImg : img}></img> */}
                     </label>
                 </div>
             </div>
+            {/* <img src={newImg ? newImg : img}></img> */}
 
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
                     <label
                         className="block text-gray-700 text-s font-bold mb-2"
+
                     >
                         Profile Picture
                         <input
@@ -150,10 +132,17 @@ export default function EditTeamMember({ id, name, img, about, position }) {
                             type="file"
                             accept="image/png, image/jpeg"
                             onChange={(e) => setFile(e.target.files?.[0])}
+                            className="appearance-none block w-72 bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                         />
                     </label>
                 </div>
             </div>
+
+            <button
+                type="button"
+                onClick={async () => check(file)}
+                className="mb-6 justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Set New Picture
+            </button>
             <button className="mb-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                 Edit Team Member
             </button>
