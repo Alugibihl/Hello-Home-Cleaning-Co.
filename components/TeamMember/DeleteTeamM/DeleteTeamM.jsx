b06318fc5f8ac164c8e1bd8b228ad1155b1918da
Corbin <corbindbullard@gmail.com>
2023-11-06 15:35:29 -0500

    fix delete

--- components/TeamMember/DeleteTeamM/DeleteTeamM.jsx
+++ components/TeamMember/DeleteTeamM/DeleteTeamM.jsx
@@ -1,36 +1,37 @@
-'use-client'
-import { HiOutlineTrash } from "react-icons/hi"
-import { useEdgeStore } from '@/libs/edgestore'
-
+"use-client";
+import { HiOutlineTrash } from "react-icons/hi";
+import { useEdgeStore } from "@/libs/edgestore";
 
 export default function DeleteTeamMemberButton({ id, setDeleted, urlD }) {
-    const { edgestore } = useEdgeStore()
-    const deleteMemeber = async () => {
-        const confirmed = confirm("Are you sure you want to delete team member?")
-        try {
-            console.log(urlD)
-            await edgestore.myPublicsImages.delete({
-                url: urlD,
-            });
-            const res = await fetch(`/api/teammembers?id=${id}`, {
-                method: "DELETE",
-            });
-            if (confirmed) {
+  const { edgestore } = useEdgeStore();
+  const deleteMemeber = async () => {
+    const confirmed = confirm("Are you sure you want to delete team member?");
+    if (confirmed) {
+      try {
+        console.log(urlD);
+        await edgestore.myPublicsImages.delete({
+          url: urlD,
+        });
+        const res = await fetch(`/api/teammembers?id=${id}`, {
+          method: "DELETE",
+        });
+        if (confirmed) {
+          const res = await fetch(`/api/teammembers?id=${id}`, {
+            method: "DELETE",
+          });
 
-                const res = await fetch(`/api/teammembers?id=${id}`, {
-                    method: "DELETE",
-                });
-
-                if (res.ok) {
-                    setDeleted(true)
-                }
-            }
-        } catch (error) { console.log("message", error) }
-
+          if (res.ok) {
+            setDeleted(true);
+          }
+        }
+      } catch (error) {
+        console.log("message", error);
+      }
     }
-    return (
-        <button onClick={deleteMemeber} >
-            <HiOutlineTrash size={24} />
-        </button>
-    )
+  };
+  return (
+    <button onClick={deleteMemeber}>
+      <HiOutlineTrash size={24} />
+    </button>
+  );
 }
